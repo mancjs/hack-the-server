d7b820e0f1dbb33af701159c2554ccca9a52cc5b
Martin Rue <hello@martinrue.com>
2013-07-03 18:37:32 +0100

    fix file size checks

--- routes/challenge4.js
+++ routes/challenge4.js
@@ -5,12 +5,11 @@ var events = require('../lib/events');
 var routes = function(app) {
   app.post('/challenge4', function(req, res) {
 
-    var stats = fs.statSync(req.files.file.path)
-    console.log(stats.size);
+    var size = fs.statSync(req.files.file.path).size;
 
     if (!req.files.file) return res.json({ error: 'no file was POSTed for key `file`' });
-    if (req.files.file.size === 42 * 1000) return res.json({ error: 'KiB?' });
-    if (req.files.file.size !== 42 * 1024) return res.json({ error: 'file is the wrong size' });
+    if (size === 42 * 1000) return res.json({ error: 'KiB?' });
+    if (size !== 42 * 1024) return res.json({ error: 'file is the wrong size' });
 
     var response = db.completeChallenge4(req.body && req.body.id);
     if (response.error) return res.json(response);
